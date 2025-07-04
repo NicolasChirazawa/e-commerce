63f0cab08c0046bbdbe5e99e7a3f2895b16f041a
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2025-07-03 23:45:03 -0300

    init: Commit inicial
    Foi estruturado a base do projeto:
    
    - Instalado as bibliotecas;
    - Estruturado banco de dados;
    - Montado a base do server em express;

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server conectado na porta ${port}`);
+})
