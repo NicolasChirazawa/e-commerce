e1e1fa9a3ab06a7343449530caaf5ddd1ae55443
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2025-07-20 22:37:08 -0300

    chore: Criação da pasta de middleware
    Ajuste dos caminhos que miravam para o controller.

--- projeto/Router/user.js
+++ projeto/Router/user.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../controllers/user.js');
-const token = require('../controllers/token.js');
+const token = require('../middleware/token.js');
 
-router.post('/registerUser',    user.registerUser);
-router.post('/login',           user.loginUser);
-router.get('/user',             token.verify_jwt, user.selectAllUsers);
-router.get('/user/:user_id',    token.verify_jwt, user.selectUser);
-router.put('/user/:user_id',    token.verify_jwt, user.updateUser);
-router.delete('/user/:user_id', token.verify_jwt, user.deleteUser);
-router.patch('/user/:user_id',  token.verify_jwt, user.patchUser);
+router.post('/registerUser',                       user.registerUser);
+router.post('/login',                              user.loginUser);
+router.get('/users',             token.verify_jwt, user.selectAllUsers);
+router.get('/users/:user_id',    token.verify_jwt, user.selectUser);
+router.put('/users/:user_id',    token.verify_jwt, user.updateUser);
+router.delete('/users/:user_id', token.verify_jwt, user.deleteUser);
+router.patch('/users/:user_id',  token.verify_jwt, user.patchUser);
 
 module.exports = router;
