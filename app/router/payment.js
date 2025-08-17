d724f9a44a11557aab4b5d99ffce584e9a0c2cbb
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2025-08-17 00:46:52 -0300

    refactor: Renomear rota de pagaento

--- app/router/payment.js
+++ app/router/payment.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const payment = require('../controllers/payment.js');
 const token = require('../middleware/token.js');
 
-router.post('/createPayment', token.verify_jwt, payment.createPaymentLink);
+router.post('/payment', token.verify_jwt, payment.createPaymentLink);
 
 module.exports = router;
